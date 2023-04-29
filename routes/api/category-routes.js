4a3623ca381d576d4ed5815781874f0873c92254
Matthew Baty <mattbaty@outlook.com>
2023-04-28 21:10:24 -0400

    post route for new category finished

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -62,6 +62,14 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  })
+  .then((tagData) => res.json(tagData))
+  .catch(err => {
+    console.log(err);
+    res.status(400).json(err);
+  })
 });
 
 router.put('/:id', (req, res) => {
