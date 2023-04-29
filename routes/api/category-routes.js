61bcd4758e9c19b0eee9688cb30540c6e82d11ee
Matthew Baty <mattbaty@outlook.com>
2023-04-28 21:19:00 -0400

    delete route finsihed, category-routes.js finished

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -90,6 +90,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(categoryData => {
+    if (!categoryData) {
+      res.status(404).json({message: 'Category with this ID does not exist'});
+      return;
+    }
+    res.json(categoryData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 module.exports = router;
